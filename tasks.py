ee904e794a8e5c78e63c9df00634db9e91fc2332
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-19 16:46:06 +0200

    Tried different approach for tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import os, sys, glob, subprocess
 from celery import Celery
+from dolfin_convert import gmsh2xml
 
 
 app = Celery('tasks', backend='amqp', broker='amqp://elias:pass@' + os.environ['master_ip'] + ':5672/geijer')
@@ -11,7 +12,10 @@ def airfoil(angle):
 	subprocess.check_call("./run.sh " + angle + " " + angle +" 1 200 1", shell=True)
 
 	print "Converting the generated mesh files to xml"
-	subprocess.check_call("./convert.sh", shell=True)
+	msh_files = glob.glob('msh/*.msh')
+	for msh in msh_files:
+		gmsh2xml(msh, msh[:-3] + 'xml')
+		print "Finished convert " + msh
 
 	print "Run airfiol on every xml file"
 	xml_files = glob.glob('msh/*.xml')
