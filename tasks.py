a2fb2ac96e1d2a91c8596ffa2a60ece1c2b75dcb
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-19 17:41:04 +0200

    Debug

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
-import os, sys, glob, subprocess
+import os, sys, glob
+from subprocess import CalledProcessError, check_output, check_call
 from celery import Celery
 from dolfin_convert import gmsh2xml
 
@@ -9,7 +10,7 @@ app = Celery('tasks', backend='amqp', broker='amqp://elias:pass@' + os.environ['
 def airfoil(angle):
 
 	print "Generate mesh files for angle " + angle
-	subprocess.check_call("./run.sh " + angle + " " + angle +" 1 200 1", shell=True)
+	check_call("./run.sh " + angle + " " + angle +" 1 200 1", shell=True)
 
 	print "Converting the generated mesh files to xml"
 	msh_files = glob.glob('msh/*.msh')
@@ -20,4 +21,8 @@ def airfoil(angle):
 	print "Run airfiol on every xml file"
 	xml_files = glob.glob('msh/*.xml')
 	for xml in xml_files:
-		subprocess.check_call("./navier_stokes_solver/airfoil 10 0.0001 10. 1 " + "./" + xml, shell=True)
+		try:
+			check_output(["./navier_stokes_solver/airfoil", "10", "0.0001", "10.", "1", "./" + xml])
+		except CalledProcessError as e:
+			print e.returncode
+
