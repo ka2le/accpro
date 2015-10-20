ac55799b980d0f3812ebc6a894da8d5db34af46b
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-20 11:32:51 +0200

    Fix

--- mastercontroller.py
+++ mastercontroller.py
@@ -24,7 +24,7 @@ def substitute(new, old, file):
 
 def start(angle_start, angle_stop, n_angles):
     angle_diff = (angle_stop-angle_start)/n_angles
-    n_workers = calc_n_workers(n_angles, 2)
+    #n_workers = calc_n_workers(n_angles, 2)
 
     master_key_pub_path = '/etc/ssh/ssh_host_rsa_key.pub'
     master_key_path = '/etc/ssh/ssh_host_rsa_key'
@@ -32,7 +32,7 @@ def start(angle_start, angle_stop, n_angles):
 
     substitute('    - export master_ip="' + master_ip +'"', 'export master_ip=', 'userdata-slave.yml')
 
-    slave_list = create_slaves(n_workers)
+    slave_list = create_slaves(n_angles)
 
     #job = group([airfoil.s(n*angle_diff) for n in range(1, n_angles+1)])
     #result = job.apply_async()
