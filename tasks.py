2bb9f0d38066ad055bb207f9f040ad80c7652dd7
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-26 17:24:05 +0100

    First try for master to analyze .m file

--- tasks.py
+++ tasks.py
@@ -3,11 +3,12 @@ from subprocess import CalledProcessError, check_output, check_call
 from celery import Celery
 from dolfin_convert import gmsh2xml
 
-
 app = Celery('tasks', backend='amqp', broker='amqp://elias:pass@' + os.environ['master_ip'] + ':5672/geijer')
 
 @app.task
 def airfoil(angle, levels):
+
+	results = []
 
 	print "Generate mesh files for angle " + str(angle)
 	check_call("./run.sh " + str(angle) + " " + str(angle) +" 1 200 " + str(levels) , shell=True)
@@ -23,10 +24,11 @@ def airfoil(angle, levels):
 	for xml in xml_files:
 		try:
 			check_call("sudo ./navier_stokes_solver/airfoil 10 0.0001 10. 1 ./" + xml, shell=True)
+			with open("results/drag_ligt.m", 'r') as f:
+				result = f.read()
+			results.append({'name':xml[4:-4], 'data':result})
+
 		except CalledProcessError as e:
 			print e.returncode
 
-	with open("results/drag_ligt.m", 'r') as f:
-		result = f.read()
-
-	return result
+	return results
--- webUI/flask.py
+++ webUI/flask.py
@@ -1,8 +1,12 @@
 #!todo-api/flask/bin/python
 from flask import Flask, request
-import sys
+import sys, celery, subprocess
 #sys.path.append("~/accpro")
 from createslaves import create_slaves
+from celery import group
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
 
@@ -26,7 +30,7 @@ def webUIBackend():
 	nodes = valuesArray[3]
 	levels = valuesArray[4]
 	#do all the stuff with the input values
-	start(startAngle, endAngle, nrAngle)
+	#start(startAngle, endAngle, nrAngle)
 	return "nodes "+str(nodes)+" startAngle "+startAngle #return something else here later
 
 @app.route('/p/<int:angle_start>,<int:angle_stop>,<int:a_n>', methods=['GET'])
@@ -35,15 +39,30 @@ def testing(angle_start, angle_stop, a_n):
 	angle_diff = (angle_stop-angle_start)/a_n
     n_workers = calc_n_workers(a_n, 1)
     slave_list = create_slaves(n_workers)
-	start(a_from, a_to, a_n)
 
-	job = group([airfoil.s(n*angle_diff,0) for n in range(1, n_angles+1)])
+	job = group([airfoil.s(n*angle_diff,0) for n in range(1, a_n+1)])
     result = job.apply_async()
 
     while result.ready() == False:
         k = 1
 
-    return result.get()
+    for r in result.get():
+    	for angle in r:
+    		name = angle['name']
+    		data = angle['data'][2:].split()[3:]
+    		time = np.array(data[::3], dtype=np.float)
+    		lift = np.array(data[1::3], dtype=np.float)
+    		drag = np.array(data[2::3], dtype=np.float)
+    		fig = plt.figure()
+    		fig.add_subplot(121)
+    		fig.set_title("Lift force")
+    		fig.plot(time, lift)
+    		fig.add_subplot(122)
+    		fig.plot(time, drag)
+    		fig.set_title("Drag force")
+    		fig.savefig(name + '.png')
+
+    return str(result.get())
 
 
 if __name__ == '__main__':
