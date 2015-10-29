55be8746e9769ac192ad6398aba0d7d66106ef84
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-29 11:49:30 +0100

    New version of front end, still very buggy

--- webUI/functions.js
+++ webUI/functions.js
@@ -1,22 +1,11 @@
-function onloading(){
+function onloading(){}
 
-}
-function startMaster(){
-	print("Starting a VM that will act as a Master...", "k");
-	document.getElementById("step1").className += " working";
-	setTimeout(function(){
-		document.getElementById("checkbox1").checked = true;
-		document.getElementById("step1").className = "step finished";
-		print("Master has been started successfully")
-	}, 2000);
-	
-}
 function handleResult(data){
 	var text = data.value; //inte exakt såhär men
 	print(text);
 }
+
 function print(text, type){
-	
 	var newHtml = document.getElementById("step3").innerHTML;
 	if(type== "h"){
 		newHtml+='<h3>'+text+'</h3>';
@@ -27,40 +16,85 @@ function print(text, type){
 		newHtml+='<p>'+text+'</p>';
 	}
 	
-	
 	document.getElementById("step3").innerHTML = newHtml;
 }
+var masterIP;
+var progress = "Just Started";
+var finished;
+var statusCheckTime = 10000;
+var counter = 0;
+
+function connectToMaster(){
+	masterIP = document.getElementById("masterIP").value;
+	checkStatus();
+	//setTimeout(function(){ checkStatus(); }, statusCheckTime);
+}
+
+function checkStatus(){
+	counter++;
+	console.log("checkStatus");
+	if(!finished){
+		setTimeout(function(){ checkStatus(); }, statusCheckTime);
+	}
+
+	$.ajax({
+		type: "GET",
+		url: "http://" + masterIP + ":5000/status",
+		success: function(data) {
+			if(data=="Complete" || counter == 30){
+				finished = true;
+			}
+			console.log(data+"<data progress>"+progress);
+			if(progress == data){
+				console.log("Nothing Happened");
+			}else{
+				var dataArray= data.split("_");
+				progress = data;
+				if(dataArray[0] = "img"){ 
+					$("#step4").append('<br><canvas id="c'+counter+'" width="800" height="600"></canvas>');
+					var canvas = document.getElementById("c"+counter);
+					var ctx = canvas.getContext("2d");
+					var image = new Image();
+					var base64String = dataArray[1];
+					image.onload = function() {
+						ctx.drawImage(this, 0, 0, canvas.width, canvas.height);
+					};
+					image.src = "data:image/  png;base64,"+base64String;
+				}else{
+					console.log(data);
+					print("Progress: "+data, "k");
+					progress = data;
+				}
+			}
+
+		}
+	});
+}
+
 function startSimulation(){
 	print("Analyzing...", "k");
+	finished = false;
 	document.getElementById("step2").className += " working";
 	var startAngle = document.getElementById("startAngle").value;
 	var endAngle = document.getElementById("endAngle").value;
 	var nrAngle = document.getElementById("nrAngle").value;
 	var nodes = document.getElementById("nodes").value;
 	var levels = document.getElementById("levels").value;
-	print("startAngle: "+ startAngle);
-	$.post("flask.php",{
-	startAngle:startAngle,
-	endAngle:endAngle,
-	nrAngle:nrAngle,
-	nodes:nodes,
-	levels:levels
-	},function(data){
+
+	$.ajax({
+	type: "GET",
+	url: "http://" + masterIP + ":5000/webUIBackend",
+	data: {
+		startAngle: startAngle,
+		endAngle: endAngle,
+		nrAngle: nrAngle,
+		nodes: nodes,
+		levels: levels
+	},
+	success: function(data) {
 		console.log(data);
+		//finished = true;
+		checkStatus();
+		}
 	});
-	/* $.ajax({
-            type: "GET",
-            url: "http://script/Root" + "/ourApp/",
-            contentType: "application/json; charset=utf-8",
-            data: { startAngle: startAngle, endAngle:endAngle, nrAngle:nrAngle, nodes:nodes, levels:levels },
-            success: function(data) {
-                printResults(data);
-            }
-        });   */   
-	
-	/* @app.route('/ourApp/', methods=['GET'])
-	def ourApp():
-		startAngle = {"value": request.args.get('startAngle')}
-		#do Stuff
-		return result */
 }
