a862bb7208d566e4d43b60323aefcdf6144d34a0
Elias Lundeqvist <elias.lundeqvist@gmail.com>
2015-10-31 15:28:26 +0100

    Fixed syntax error

--- tasks.py
+++ tasks.py
@@ -10,17 +10,21 @@ app.conf.CELERYD_PREFETCH_MULTIPLIER = 1
 
 # Takes one angle as input as an integer, which will generate mesh files, convert them to xml and then run airfoil on them
 @app.task
-def airfoil(angle, nodes, levels):
-	subprocess.check_call("sudo rm msh/*a" + str(angle) + "*", shell=True)
-	subprocess.check_call("sudo rm geo/*a" + str(angle) + "*", shell=True)
+def airfoil(i_angle, i_nodes, i_levels):
+	angle = str(i_angle)
+	nodes = str(i_nodes)
+	levels = str(i_levels)
+
+	check_call("sudo rm msh/*a" + angle + "*", shell=True)
+	check_call("sudo rm geo/*a" + angle + "*", shell=True)
 	
 	results = []
 
-	print "Generate mesh files for angle " + str(angle)
-	check_call("./run.sh " + str(angle) + " " + str(angle) +" 1 " + str(nodes) + " " + str(levels) , shell=True)
+	print "Generate mesh files for angle " + angle
+	check_call("./run.sh " + angle + " " + angle +" 1 " + nodes + " " + levels , shell=True)
 
 	print "Converting the generated mesh files to xml"
-	msh_files = glob.glob('msh/r*a' + str(angle) + 'n*.msh')
+	msh_files = glob.glob('msh/r*a' + angle + 'n*.msh')
 	for msh in msh_files:
 		gmsh2xml(msh, msh[:-3] + 'xml')
 		print "Finished convert " + msh
@@ -30,7 +34,7 @@ def airfoil(angle, nodes, levels):
 		#print "Deleted geo: geo/" + msh[6:-3] + 'geo'
 
 	print "Run airfiol on every xml file"
-	xml_files = glob.glob('msh/a*' + str(angle) + 'n*.xml')
+	xml_files = glob.glob('msh/a*' + angle + 'n*.xml')
 	for xml in xml_files:
 		try:
 			check_call("sudo ./navier_stokes_solver/airfoil 10 0.0001 10. 0.2 ./" + xml, shell=True)
